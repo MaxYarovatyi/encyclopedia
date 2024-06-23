465f150739d5b5b27181d66aed75423a4f1e244f
makcyarovatij@gmail.com <makcyarovatij@gmail.com>
2024-06-23 16:15:28 +0300

    First commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,113 +1,138 @@
-var searchField = document.getElementById('processor-name');
-searchField.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      document.getElementById("filter-button").click();
-    }
+var searchField = document.getElementById("storageDevice-name");
+searchField.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("filter-button").click();
+  }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const processorsList = document.querySelector('.processors-list')
-    const urlParams = new URLSearchParams(window.location.search);
-    const processorId = urlParams.get('id');
+document.addEventListener("DOMContentLoaded", function () {
+  const storageDevicesList = document.querySelector(".storageDevices-list");
+  const urlParams = new URLSearchParams(window.location.search);
+  const storageDeviceId = urlParams.get("id");
+  addImage();
 
-    function displayProcessors(processors, filters) {
-        processorsList.innerHTML = '';
-        processors.forEach(processor => {
-            const card = document.createElement('div');
-            card.className = 'processor-card';
-            card.innerHTML = `
-                <img src="${processor.image}" alt="${processor.name}">
-                <h3>${processor.name}</h3>
-                <p>Набір команд: ${processor.instructionSet}</p>
-                <p>Кількість ядер: ${processor.cores}</p>
-                <p>Виробник: ${processor.manufacturer}</p>
-                <p>Тактова частота: ${processor.frequency} ГГц</p>
-                <a href="processor.html?id=${processor.id}&name=${filters.name}&instructionSet=${filters.instructionSet}&cores=${filters.cores}&manufacturer=${filters.manufacturer}&frequency=${filters.frequency}">Детальніше</a>
-            `;
-            processorsList.appendChild(card);
-        });
+  function displayStorageDevices(storageDevices, filters) {
+    storageDevicesList.innerHTML = "";
+    console.log(storageDevices, storageDevicesList);
+    storageDevices.forEach((storageDevice) => {
+      const name = storageDevice.name;
+      const card = document.createElement("div");
+      card.className = "storageDevice-card";
+      card.innerHTML = `
+            <img src="${storageDevice.image}" alt="${storageDevice.name}">
+            <h3>${storageDevice.name}</h3>
+            <p>Ємність: ${storageDevice.capacity}</p>
+            <p>Інтерфейс: ${storageDevice.interface}</p>
+            <p>Виробник: ${storageDevice.manufacturer}</p>
+            <p>Швидкість передачі даних: ${storageDevice.transferRate}</p>
+            <a href="storageDevice.html?id=${storageDevice.id}&name=${storageDevice.name}&capacity=${storageDevice.capacity}&interface=${storageDevice.interface}&manufacturer=${storageDevice.manufacturer}&transferRate=${storageDevice.transferRate}">Детальніше</a>
+          `;
+      storageDevicesList.appendChild(card);
+    });
 
-        if (processorsList.innerHTML == '') {
-            processorsList.innerHTML = `
-                <b class="no-results">ПО ВАШОМУ ЗАПИТУ НЕ ЗНАЙДЕНО ЖОДНОГО МІКРОПРОЦЕСОРА</b>
-            `;
-        }
+    if (storageDevicesList.innerHTML == "") {
+      storageDevicesList.innerHTML = `
+            <b class="no-results">ПО ВАШОМУ ЗАПИТУ НІЧОГО НЕ ЗНАЙДЕНО</b>
+          `;
     }
+  }
+  function filterStorageDevices() {
+    const name = document.getElementById("storageDevice-name").value;
+    const capacity = document.getElementById("storageDevice-capacity").value;
+    const interface = document.getElementById("storageDevice-interface").value;
+    const manufacturer = document.getElementById(
+      "storageDevice-manufacturer"
+    ).value;
 
-    function filterProcessors() {
-        const name = document.getElementById('processor-name').value;
-        const instructionSet = document.getElementById('instruction-set').value;
-        const cores = document.getElementById('cores').value;
-        const manufacturer = document.getElementById('manufacturer').value;
-        const frequency = document.getElementById('frequency').value;
+    const filters = { name, capacity, interface, manufacturer };
+    console.log(filters);
+    const filteredStorageDevices = storageDevices.filter((storageDevice) => {
+      return (
+        (name === "all" || storageDevice.name.includes(name)) &&
+        (capacity === "all" || storageDevice.capacity == capacity) &&
+        (interface === "all" || storageDevice.interface === interface) &&
+        (manufacturer === "all" || storageDevice.manufacturer === manufacturer)
+      );
+    });
 
-        const filters = { name, instructionSet, cores, manufacturer, frequency };
+    displayStorageDevices(filteredStorageDevices, filters);
+  }
 
-        const filteredProcessors = processors.filter(processor => {
-            return (name === 'all' || processor.name.includes(name)) &&
-                   (instructionSet === 'all' || processor.instructionSet === instructionSet) &&
-                   (cores === 'all' || processor.cores >= cores) &&
-                   (manufacturer === 'all' || processor.manufacturer === manufacturer) &&
-                   (frequency === 'all' || processor.frequency <= frequency);
-        });
+  function displayStorageDeviceDetails(storageDevice) {
+    console.log(
+      storageDevice,
+      document.getElementById("storageDevice-capacity")
+    );
+    document.getElementById("storageDevice-name").textContent =
+      storageDevice.name;
+    document.getElementById("storageDevice-image").src = storageDevice.image;
+    document.getElementById("storageDevice-image").alt = storageDevice.name;
+    document.getElementById("storageDevice-capacity").textContent =
+      storageDevice.capacity;
+    document.getElementById("storageDevice-interface").textContent =
+      storageDevice.interface;
+    document.getElementById("storageDevice-manufacturer").textContent =
+      storageDevice.manufacturer;
+    document.getElementById("storageDevice-transferRate").textContent =
+      storageDevice.transferRate;
+    document.getElementById("storageDevice-description").innerHTML =
+      storageDevice.description;
 
-        displayProcessors(filteredProcessors, filters);
+    const name = urlParams.get("name") || "all";
+    const capacity = urlParams.get("capacity") || "all";
+    const interface = urlParams.get("interface") || "all";
+    const manufacturer = urlParams.get("manufacturer") || "all";
+    const transferRate = urlParams.get("transferRate") || "all";
+
+    const backLink = document.getElementById("back-to-list");
+    backLink.href = `main1.html?name=${name}&capacity=${capacity}&interface=${interface}&manufacturer=${manufacturer}&transferRate=${transferRate}`;
+  }
+
+  function restoreFilters() {
+    const name = urlParams.get("name") || "all";
+    const capacity = urlParams.get("capacity") || "all";
+    const interface = urlParams.get("interface") || "all";
+    const manufacturer = urlParams.get("manufacturer") || "all";
+    const transferRate = urlParams.get("transferRate") || "all";
+
+    document.getElementById("storageDevice-name").value =
+      name == "all" ? "" : name;
+    document.getElementById("storageDevice-capacity").value = capacity;
+    document.getElementById("storageDevice-interface").value = interface;
+    document.getElementById("storageDevice-manufacturer").value = manufacturer;
+    //document.getElementById("storageDevice-transferRate").value = transferRate;
+
+    filterStorageDevices();
+  }
+
+  if (storageDevicesList) {
+    restoreFilters();
+    document
+      .getElementById("filter-button")
+      .addEventListener("click", function () {
+        const name = document.getElementById("storageDevice-name").value;
+        const capacity = document.getElementById(
+          "storageDevice-capacity"
+        ).value;
+        const interface = document.getElementById(
+          "storageDevice-interface"
+        ).value;
+        const manufacturer = document.getElementById(
+          "storageDevice-manufacturer"
+        ).value;
+
+        window.history.pushState(
+          {},
+          "",
+          `main1.html?name=${name}&capacity=${capacity}&interface=${interface}&manufacturer=${manufacturer}`
+        );
+        filterStorageDevices();
+      });
+  } else if (storageDeviceId) {
+    const storageDevice = storageDevices.find((sd) => sd.id == storageDeviceId);
+    if (storageDevice) {
+      displayStorageDeviceDetails(storageDevice);
     }
-
-    function displayProcessorDetails(processor) {
-        document.getElementById('processor-name').textContent = processor.name;
-        document.getElementById('processor-image').src = processor.image;
-        document.getElementById('processor-image').alt = processor.name;
-        document.getElementById('processor-instruction-set').textContent = processor.instructionSet;
-        document.getElementById('processor-cores').textContent = processor.cores;
-        document.getElementById('processor-manufacturer').textContent = processor.manufacturer;
-        document.getElementById('processor-frequency').textContent = processor.frequency;
-        document.getElementById('processor-description').innerHTML = processor.description;
-        document.getElementById('processor-iframe').src = processor.video;
-
-        const name = urlParams.get('name') || 'all';
-        const instructionSet = urlParams.get('instructionSet') || 'all';
-        const cores = urlParams.get('cores') || 'all';
-        const manufacturer = urlParams.get('manufacturer') || 'all';
-        const frequency = urlParams.get('frequency') || 'all';
-
-        const backLink = document.getElementById('back-to-list');
-        backLink.href = `main.html?name=${name}&instructionSet=${instructionSet}&cores=${cores}&manufacturer=${manufacturer}&frequency=${frequency}`;
-    }
-
-    function restoreFilters() {
-        const name = urlParams.get('name') || 'all';
-        const instructionSet = urlParams.get('instructionSet') || 'all';
-        const cores = urlParams.get('cores') || 'all';
-        const manufacturer = urlParams.get('manufacturer') || 'all';
-        const frequency = urlParams.get('frequency') || 'all';
-
-        document.getElementById('processor-name').value = name == 'all' ? '' : name;
-        document.getElementById('instruction-set').value = instructionSet;
-        document.getElementById('cores').value = cores;
-        document.getElementById('manufacturer').value = manufacturer;
-        document.getElementById('frequency').value = frequency;
-
-        filterProcessors();
-    }
-
-    if (processorsList) {
-        restoreFilters();
-        document.getElementById('filter-button').addEventListener('click', function() {
-            const name = document.getElementById('processor-name').value
-            const instructionSet = document.getElementById('instruction-set').value;
-            const cores = document.getElementById('cores').value;
-            const manufacturer = document.getElementById('manufacturer').value;
-            const frequency = document.getElementById('frequency').value;
-
-            window.history.pushState({}, '', `main.html?name=${name}&instructionSet=${instructionSet}&cores=${cores}&manufacturer=${manufacturer}&frequency=${frequency}`);
-            filterProcessors();
-        });
-    } else if (processorId) {
-        const processor = processors.find(p => p.id == processorId);
-        if (processor) {
-            displayProcessorDetails(processor);
-        }
-    }
+  }
 });
